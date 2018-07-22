ca125cbe8d05fe9547bb119e0209270aca88fbbc
gchu <gchu647@gmail.com>
2018-07-21 19:31:40 -1000

    Set up condtion statements for google.com and localhost

--- client.js
+++ client.js
@@ -1,25 +1,39 @@
 let net = require('net');
-let requestMethod = process.argv[2];
-let requestURI = process.argv[3]; // Need to build in if statement to see if there is a GET, HEAD or nothing.
-let requestHeader = `${requestMethod} ${requestURI} HTTP/1.1
+//Need to add in an elseif for HEAD
+if(process.argv[2] === 'GET') {
+  var requestMethod = process.argv[2];;
+  var requestURI = process.argv[3];
+
+} else { //When a method is not enterd
+  var requestMethod = 'GET';
+  var requestURI = process.argv[2];
+}
+
+
+// Set different port and host for localhost and other web server
+if(requestURI.indexOf('localhost') > -1) {
+  var port = 8640;
+  var host = "localhost";
+} else if(requestURI.indexOf('www') > -1) {
+  var port = 80;
+  var host = requestURI;
+}
+
+// Need to update url instead of "/" FIX THIS!!!!!
+let requestHeader = `${requestMethod} / HTTP/1.1
 Date: ${new Date()}
-Host: ${requestURI}
+Host: ${host}
 Connection: keep-alive
 User-Agent: client.js
 
 `;
-// Working on making our own port
-let port = 8640;
 
-if(requestURI.indexOf('www') > -1) {
-  port = 80;
-}
+console.log('port ', port);
+console.log('requestMethod: ', requestMethod );
+console.log('requestURI:', requestURI);
+console.log('host: ', host);
 
-console.log("port ", port);
-console.log(process.argv[2]);
-console.log(process.argv[3]);
-
-client = net.connect(port, requestURI, function(){
+client = net.connect(port, host, function(){
   process.stdout.write('Connected to server!');
   client.write(requestHeader);
 });
